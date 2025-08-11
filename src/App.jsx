18e847cfab24eb1517573f48888cab628cb3e686
Jatin Manhotra <151845467+JatinManhotra@users.noreply.github.com>
2025-08-11 11:14:51 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,18 @@
 import React from "react";
 import Dashboard from "./pages/Dashboard/Dashboard";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Transaction from "./pages/Transaction/Transaction";
 import Support from "./pages/Support/Support";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Dashboard />,
-    },
-    {
-      path: "/transaction",
-      element: <Transaction />,
-    },
-    {
-      path: "/support",
-      element: <Support />,
-    },
-  ]);
-
   return (
-    <>
-      <RouterProvider router={router} />
-    </>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/transaction" element={<Transaction />} />
+        <Route path="/support" element={<Support />} />
+      </Routes>
+    </HashRouter>
   );
 };
 
